97decbe896fc05e78f418feccd16a0a8c09e4ba7
Facundo Andrioli Villa <facundoo30@gmail.com>
2025-10-01 15:08:31 -0300

    pytests

---
+++ python_app/test/test_app.py
@@ -0,0 +1,10 @@
+from python_app import app
+
+def test_sumar():
+    assert app.sumar(2, 3) == 5
+
+def test_es_par_true():
+    assert app.es_par(4) is True
+
+def test_es_par_false():
+    assert app.es_par(5) is False
